9eb4595d4c84dca8747a7b36d375023bf4dd2cea
JunJie Yong <13977709+JunJieYong@users.noreply.github.com>
2022-03-12 00:40:03 +0800

    Fix prefix

--- src/commands/config/prefix.ts
+++ src/commands/config/prefix.ts
@@ -6,17 +6,17 @@ export const description = 'Change the prefix of the bot';
 export const aliases = ['p'];
 export const usage = 'prefix <prefix>';
 export const excute = (args: string[], message: Message) => {
-  if (!args[0]) {
-    message.reply(`The current prefix is \`${process.env.PREFIX}\``);
-  } else {
-    if (message.guildId) {
-      const guild = GuildConfig.getConfig(message.guildId);
-      if (guild) {
+  if (message.guildId) {
+    const guild = GuildConfig.getConfig(message.guildId);
+    if (guild) {
+      if (args[0]) {
         guild.setPrefix(args[0]);
         message.reply(`The prefix has been changed to \`${args[0]}\``);
+      } else {
+        message.reply(`The prefix is \`${guild.getPrefix()}\``);
       }
-    } else {
-      message.reply(`The prefix can only be changed in a server`);
     }
+  } else {
+    message.reply(`The prefix can only be changed in a server`);
   }
 };
