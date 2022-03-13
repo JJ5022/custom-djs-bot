76e315b7399dc9aa59823bf83418ce479ce552db
JunJie Yong <13977709+JunJieYong@users.noreply.github.com>
2022-03-13 22:45:21 +0800

    Shift Discord.js event array to a seperate file

--- src/events/index.test.ts
+++ src/events/index.test.ts
@@ -1,8 +1,6 @@
 import path from 'path';
+import { djsEvent, djsDeprecatedEvent } from '../utils/djsEvent';
 import getModules from '../utils/getModules';
-
-//prettier-ignore
-const allEventName = ["apiRequest","apiResponse","applicationCommandCreate","applicationCommandDelete","applicationCommandUpdate","channelCreate","channelDelete","channelPinsUpdate","channelUpdate","debug","emojiCreate","emojiDelete","emojiUpdate","error","guildBanAdd","guildBanRemove","guildCreate","guildDelete","guildIntegrationsUpdate","guildMemberAdd","guildMemberAvailable","guildMemberRemove","guildMembersChunk","guildMemberUpdate","guildScheduledEventCreate","guildScheduledEventDelete","guildScheduledEventUpdate","guildScheduledEventUserAdd","guildScheduledEventUserRemove","guildUnavailable","guildUpdate","interaction","interactionCreate","invalidated","invalidRequestWarning","inviteCreate","inviteDelete","message","messageCreate","messageDelete","messageDeleteBulk","messageReactionAdd","messageReactionRemove","messageReactionRemoveAll","messageReactionRemoveEmoji","messageUpdate","presenceUpdate","rateLimit","ready","roleCreate","roleDelete","roleUpdate","shardDisconnect","shardError","shardReady","shardReconnecting","shardResume","stageInstanceCreate","stageInstanceDelete","stageInstanceUpdate","stickerCreate","stickerDelete","stickerUpdate","threadCreate","threadDelete","threadListSync","threadMembersUpdate","threadMemberUpdate","threadUpdate","typingStart","userUpdate","voiceStateUpdate","warn","webhookUpdate"];
 
 describe('Testing event', () => {
   it('should be able to import event', async () => {
@@ -11,7 +9,8 @@ describe('Testing event', () => {
       const event = await import(`${module}`);
       expect(event.name).toBeDefined();
       expect(event.default).toBeDefined();
-      expect(allEventName.includes(event.name)).toBeTruthy();
+      expect(djsEvent.includes(event.name)).toBeTruthy();
+      expect(djsDeprecatedEvent.includes(event.name)).toBeFalsy();
     }
   });
 });
