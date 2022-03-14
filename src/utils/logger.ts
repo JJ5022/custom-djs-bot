d94bd4a0d67fe7d0526b78f7b795c32bf8260a05
JunJie Yong <13977709+JunJieYong@users.noreply.github.com>
2022-03-14 09:25:37 +0800

    Change: utils/logger -Added profiler formatting -Added file logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,8 @@ import { createLogger, format } from 'winston';
 import {
   Console,
   ConsoleTransportOptions,
+  File,
+  FileTransportOptions,
 } from 'winston/lib/winston/transports';
 
 export enum LogLevel {
@@ -14,16 +16,33 @@ export enum LogLevel {
   silly = 'silly',
 }
 
+const profilerFormat = format(info => {
+  if (info.durationMs !== undefined) {
+    info.message = `${info.message} (${info.durationMs}ms)`;
+  }
+  return info;
+});
+
 const consoleOption: ConsoleTransportOptions = {
   silent: false,
   level: LogLevel.silly,
-  format: format.cli({
-    all: true,
-  }),
+  format: format.combine(
+    profilerFormat(),
+    format.cli({
+      all: true,
+    })
+  ),
+};
+
+const fileOption: FileTransportOptions = {
+  silent: false,
+  level: LogLevel.silly,
+  format: format.json(),
+  filename: 'logs/output.log',
 };
 
 export const logger = createLogger({
-  transports: [new Console(consoleOption)],
+  transports: [new Console(consoleOption), new File(fileOption)],
 });
 
 export default logger;
